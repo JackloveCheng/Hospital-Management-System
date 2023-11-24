0302928df8221720a57686bbfa55baeca5c70434
Neuocell <131649687+Neuocell@users.noreply.github.com>
2023-11-24 19:47:58 +0800

    1

---
+++ templates/hospital/search.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Search results</title>
+    <meta charset="utf-8">    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+    <meta name = "format-detection" content = "telephone=no" />
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">
+    <link rel="stylesheet" href="css/bootstrap.css" >
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="fonts/font-awesome.css">
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="search/search.js"></script>
+    <script src="js/jquery-migrate-1.2.1.js"></script>
+    <script  src="js/touchTouch.jquery.js"></script>
+    <script src="js/superfish.js"></script>
+    <script src="js/jquery.mobilemenu.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/jquery.ui.totop.js"></script>
+    <script src="js/jquery.touchSwipe.min.js"></script>
+    <script src="js/jquery.equalheights.js"></script>
+    
+      <!--[if lt IE 9]>
+        <div style='text-align:center'><a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." /></a></div>  
+      <![endif]-->
+      <!--[if lt IE 9]><script src="../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    
+      <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+      <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+      <![endif]-->
+</head>
+
+<body>
+<!--==============================header=================================-->
+<header id="header">
+    <article class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <h1 class="navbar-brand navbar-brand_"><a href="index.html"><img alt="Vedi" src="img/logo.png"></a></h1> 
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <p class="headeraddress1">
+                    <img src="img/page1_icon1.png" alt=""> 
+                    <span>Your first visit? Call for info</span>
+                    <strong>+1-234-567-8900</strong>
+                </p> 
+            </div>
+        </div>
+    </article>
+    <article class="container">
+        <div class="menuheader">
+            <nav class="navbar navbar-default navbar-static-top tm_navbar" role="navigation">
+                <ul class="nav sf-menu">
+                  <li><a href="index.html">Home</a></li>
+                  <li><a href="index-1.html">Services</a></li>
+                  <li><a href="index-2.html">About us<em class="indicator1"></em></a>
+                    <ul>
+                      <li><a href="#">Dolore ipsu</a></li>
+                      <li><a href="#">Consecte<em class="indicator2"></em></a>
+                        <ul>
+                           <li><img src="img/arrowsubmenu1.png" alt=""><a href="#">Dolore ipsu</a></li>
+                           <li><a href="#">Consecte</a></li> 
+                           <li><a class="last" href="#">Elit Conseq</a></li>                       
+                        </ul>
+                      </li>
+                      <li><a class="last" href="#">Elit Conseq</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="index-3.html">Blog</a></li>
+                  <li class="last active"><a href="index-4.html">Contacts</a></li>
+                </ul>
+            </nav>
+        </div>
+    </article>
+</header>
+<!--============================== row_10 =================================-->
+<section class="row_10">
+      <div class="container">
+            <div class="row">
+              <article class="col-lg-12 col-md-12 col-sm-12">
+               <h2 class="h2-pad">Search result:</h2>
+                <div id="search-results"></div>
+              </article>
+            </div>
+      </div>
+</section>
+<!--==============================footer=================================-->
+<footer>
+    <div class="container">
+        <div class="footerrow1">
+            <div class="row">
+                <article class="col-lg-6 col-md-6 col-sm-6 col-lg-push-6 col-md-push-6 col-sm-push-6 colfooterrow1">
+                    <ul class="social2 clearfix">
+                         <li><a href="#"><img src="img/social_icon1.png" alt=""></a></li>
+                         <li><a href="#"><img src="img/social_icon2.png" alt=""></a></li>
+                         <li><a href="#"><img src="img/social_icon3.png" alt=""></a></li>
+                         <li><a href="#"><img src="img/social_icon4.png" alt=""></a></li>
+                    </ul> 
+               </article>
+                <article class="col-lg-6 col-md-6 col-sm-6 col-lg-pull-6 col-md-pull-6 col-sm-pull-6 colfooterrow1">
+                    <p class="footerpriv"><a href="index.html">Vedi </a> &copy; <span id="copyright-year"></span> <img src="img/linefooter.jpg" alt=""> <strong><a class="privacylink" href="index-5.html">Privacy Policy</a></strong></p>
+                </article>
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/tm-scripts.js"></script>
+</body>
+</html>
