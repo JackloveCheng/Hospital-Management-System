9819aa5106853b166795555eebe65f3c23b67408
ganxuan <1320007169@qq.com>
2023-12-04 15:33:50 +0800

    1
    1

--- hospitalmanagement/urls.py
+++ hospitalmanagement/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
     path('doctorsignup', views.doctor_signup_view,name='doctorsignup'),
     path('patientsignup', views.patient_signup_view,name='patientsignup'),
     
-    path('adminlogin', LoginView.as_view(template_name='hospital/patientlogin.html')),
+    path('adminlogin', LoginView.as_view(template_name='hospital/adminlogin.html')),
     path('doctorlogin', LoginView.as_view(template_name='hospital/doctorlogin.html')),
     path('patientlogin', LoginView.as_view(template_name='hospital/patientlogin.html')),
 
